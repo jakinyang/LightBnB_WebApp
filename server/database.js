1e9935dc0b89ae24352512194d3ebdc8412c143f
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-11-17 16:09:03 -0800

    Add functionality to add property listings

--- server/database.js
+++ server/database.js
@@ -180,9 +180,32 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = [
+    property.owner_id, 
+    property.title, 
+    property.thumbnail_photo_url, 
+    property.cover_photo_url, 
+    property.cost_per_night, 
+    property.parking_spaces, 
+    property.number_of_bathrooms, 
+    property.number_of_bedrooms, 
+    property.country, 
+    property.street, 
+    property.city, 
+    property.province, 
+    property.post_code
+  ];
+  return pool.query(
+    `INSERT INTO properties (owner_id, title, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+    RETURNING *;
+    `, queryParams
+  )
+  .then((result) => {
+    return result.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 }
 exports.addProperty = addProperty;
